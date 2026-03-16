eff56eff1ff2b66b5c99382dece65a8e27b341ca
Moskvin Ivan <101857470+HotGodDog@users.noreply.github.com>
2026-03-16 17:16:56 +0800

    Add files via upload

---
+++ Array/Array.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+const int DEFAULT_CAPACITY = 10;
+
+class ArrayException {};
+
+class Array
+{
+	int* ptr;
+	int size, capacity;
+
+public:
+	int& operator [] (int index);
+	void insert(int elem, int index);
+	void insert(int elem);
+	void increaseCopacity(int newCapacity);
+	void remove(int index);
+
+	//int get(int index) const;
+	//void set(int index, int elem);
+
+	int getSize() const;
+	friend ostream& operator << (ostream& out, const Array& arr);
+
+	explicit Array(int startCapacity = DEFAULT_CAPACITY);
+
+	~Array();
+	Array(const Array& arr);
+	Array& operator = (const Array& arr);
+
+};
