6f370b3ec658ddbe8b9cde499ca91925f2c0e4e7
Moskvin Ivan <101857470+HotGodDog@users.noreply.github.com>
2026-03-17 10:21:41 +0800

    Add files via upload

--- Rational/main.cpp
+++ Rational/main.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
+int sqrt(Rational a);
+void quadr_equat(Rational a, Rational b, Rational c);
+
 int main()
 {
     setlocale(LC_ALL, "RU");
 
     Rational a = 1, b = -1, c = -12;
 
-    //cout << "Р РµС€РµРЅРёРµ РєРІР°РґСЂР°С‚РЅРѕРіРѕ СѓСЂР°РІРЅРµРЅРёСЏ ax^2 + bx + c = 0" << endl;
-    //cout << "Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ a, b, c: ";
+    //cout << "Решение квадратного уравнения ax^2 + bx + c = 0" << endl;
+    //cout << "Введите коэффициенты a, b, c: ";
     //cin >> a >> b >> c;
 
     //test();
@@ -21,4 +24,45 @@ int main()
     //cout << 2 / Rational (1, 2) << endl;
 
     return 0;
+}
+
+int sqrt(Rational a)
+{
+    Rational x = a;
+    int counter = 0;
+    cout << "x" << counter << " = " << x << endl;
+    while (x.num < INT_MAX / x.den) {
+        x = (x + (a / x)) / 2;
+        counter++;
+        cout << "x" << counter << " = " << x << endl;
+        if (x == a)
+            break;
+    }
+    return x;
+}
+
+void quadr_equat(Rational a, Rational b, Rational c)
+{
+    cout << "\nУравнение: (" << a << ")x^2 + (" << b << ")x + " << c << " = 0" << endl;
+
+    Rational d = (b * b) + -4 * a * c;
+
+    cout << "Дискриминант: " << d << endl;
+
+    Rational d_sqrt = sqrt(d);
+
+    cout << "Корень: " << d_sqrt << endl;
+
+    if (d > 0) {
+        Rational x1 = (-b + d_sqrt) / (2 * a);
+        Rational x2 = (-b + -d_sqrt) / (2 * a);
+        cout << "Два корня: x1 = " << x1 << ", x2 = " << x2 << endl;
+    }
+    else if (d == 0) {
+        Rational x = -b / (2 * a);
+        cout << "Один корень: x = " << x << endl;
+    }
+    else {
+        cout << "Действительных корней нет" << endl;
+    }
 }
