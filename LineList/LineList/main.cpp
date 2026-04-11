48d441c4c721a0158b5a9e03e361dcaca5f94e7c
Ivan <moskvinuwu@gmail.com>
2026-04-11 16:36:07 +0800

    Testing commit

---
+++ LineList/LineList/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "LineList.h"
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	LineList<int> list;
+
+	cout << "Начало: " << list << endl;
+
+	list.insertFirst(10);
+
+	LineListElem<int>* ptr = list.getStart();
+
+	list.insertAfter(ptr, 15);
+	list.insertAfter(ptr->getNext(), 12);
+	list.insertFirst(7);
+
+	cout << "Шаг 1: " << list << endl;
+
+	LineListElem<int>* last = list.getLast();
+	list.insertAfter(last, 40);
+	cout << "После вставки 40 в конец: " << list << endl;
+
+	return 0;
+}
